0ae30e4bd94b577bc2cc5c822e007e42250a710a
jefferson.brandao <jefferson.brandao@g.globo>
2024-08-29 20:49:59 -0300

    criando join page para entrar em novos workspaces

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -8,6 +8,44 @@ const generateCode = () => {
   return code;
 }
 
+export const join = mutation({
+  args: {
+    joinCode: v.string(),
+    workspaceId: v.id("workspaces")
+  },
+  handler: async (ctx, args) => {
+    const userId = await auth.getUserId(ctx);
+
+    if (!userId) {
+      throw new Error("Sem permissão");
+    }
+
+    const workspace = await ctx.db.get(args.workspaceId);
+
+    if (!workspace) {
+      throw new Error("Workspace não encontrada");
+    }
+
+    if (workspace.joinCode !== args.joinCode.toUpperCase()) {
+      throw new Error("Código de convite inválido");
+    }
+
+    const existingMember = await ctx.db.query("members").withIndex("by_workspace_id_user_id", (q) => q.eq("workspaceId", args.workspaceId).eq("userId", userId)).unique();
+
+    if (existingMember) {
+      throw new Error("Você já faz parte desta workspace");
+    }
+
+    await ctx.db.insert("members", {
+      userId,
+      workspaceId: workspace._id,
+      role: "member"
+    })
+
+    return workspace._id
+  }
+})
+
 export const newJoinCode = mutation({
   args: {
     workspaceId: v.id("workspaces")
@@ -161,5 +199,25 @@ export const remove = mutation({
     await ctx.db.delete(args.id)
 
     return args.id
+  }
+})
+
+export const getInfoById = query({
+  args: { id: v.id("workspaces") },
+  handler: async (ctx, args) => {
+    const userId = await auth.getUserId(ctx);
+
+    if (!userId) {
+      return null
+    }
+
+    const member = await ctx.db.query("members").withIndex("by_workspace_id_user_id", (q) => q.eq("workspaceId", args.id).eq("userId", userId)).unique();
+
+    const workspace = await ctx.db.get(args.id);
+
+    return {
+      name: workspace?.name,
+      isMember: !!member
+    }
   }
 })
---
+++ src/app/join/[workspaceId]/page.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import { useEffect, useMemo } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { useGetWorkspaceInfo } from "@/features/workspaces/api/use-get-workspace-info";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import VerificationInput from "react-verification-input";
+import { Loader } from "lucide-react";
+import { useJoin } from "@/features/workspaces/api/use-join";
+import { toast } from "sonner";
+import { cn } from "@/lib/utils";
+
+const JoinPage = () => {
+  const router = useRouter();
+  const workspaceId = useWorkspaceId();
+
+  const { data, isLoading } = useGetWorkspaceInfo({ id: workspaceId });
+  const { mutate, isPending } = useJoin();
+
+  const isMember = useMemo(() => data?.isMember, [data?.isMember]);
+
+  useEffect(() => {
+    if (isMember) {
+      router.push(`/workspace/${workspaceId}`);
+    }
+  }, [isMember, router, workspaceId]);
+
+  const handleComplete = (value: string) => {
+    mutate(
+      { workspaceId, joinCode: value },
+      {
+        onSuccess: (id) => {
+          router.replace(`/workspace/${id}`);
+          toast.success("Você entrou no workspace com sucesso");
+        },
+        onError: () => {
+          toast.error("Código de convite inválido.");
+        },
+      }
+    );
+  };
+
+  if (isLoading) {
+    return (
+      <div className="h-full flex items-center justify-center">
+        <Loader className="size-6 animate-spin text-muted-foreground" />
+      </div>
+    );
+  }
+
+  return (
+    <div className="h-full flex flex-col gap-y-8 items-center justify-center bg-white p-8 rounded-lg shadow-sm">
+      <Image src="/logo.svg" alt="Logo slack" width={60} height={60} />
+      <div className="flex flex-col gap-y-4 items-center justify-center max-w-md">
+        <div className="flex flex-col gap-y-2 items-center justify-center">
+          <h1 className="text-2xl font-bold">Entre no {data?.name}</h1>
+          <p className="text-md text-muted-foreground">
+            Informe o código do Workspace
+          </p>
+        </div>
+
+        <VerificationInput
+          onComplete={handleComplete}
+          length={6}
+          classNames={{
+            container: cn(
+              "flex gap-x-2",
+              isPending && "opacity-50 cursor-not-allowed"
+            ),
+            character:
+              "uppercase h-auto rounded-md border border-gray-300 flex items-center justify-center text-lg font-medium text-gray-400",
+            characterInactive: "bg-muted",
+            characterSelected: "bg-white text-black",
+            characterFilled: "bg-white text-black",
+          }}
+          autoFocus
+        />
+      </div>
+
+      <div className="flex gap-x-4">
+        <Button size={"lg"} variant={"outline"} asChild>
+          <Link href={"/"}>Voltar</Link>
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default JoinPage;
